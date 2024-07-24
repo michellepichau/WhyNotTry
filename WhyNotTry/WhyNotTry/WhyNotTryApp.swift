668ea6088f71c7e507c43096e3f77890c4e33482
michellepichau <50593955+michellepichau@users.noreply.github.com>
2024-07-24 14:55:07 -0300

    Add files via upload

---
+++ WhyNotTry/WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Michelle Pichau on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
